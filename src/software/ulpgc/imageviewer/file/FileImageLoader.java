1bb18797e58c9c0b12e6cba1d1cb5f3a4e86931f
Nahuel Cosme <ncdv.2003@gmail.com>
2024-01-19 20:42:47 +0000

    FileImageLoader class created

---
+++ src/software/ulpgc/imageviewer/file/FileImageLoader.java
@@ -0,0 +1,47 @@
+package software.ulpgc.imageviewer.file;
+
+import software.ulpgc.imageviewer.Image;
+import software.ulpgc.imageviewer.ImageLoader;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.util.Set;
+
+public class FileImageLoader implements ImageLoader {
+    private final File[] files;
+    private static final Set<String> imageExtensions = Set.of(".png", ".jpg");
+
+    public FileImageLoader(File folder) {
+        this.files = folder.listFiles(isImage());
+    }
+
+    private FilenameFilter isImage() {
+        return (dir, name) -> imageExtensions.stream()
+                .anyMatch(name::endsWith);
+    }
+
+    @Override
+    public Image load() {
+        return imageAt(0);
+    }
+
+    private Image imageAt(int i) {
+        return new Image() {
+            @Override
+            public String name() {
+                return files[i].getAbsolutePath();
+            }
+
+            @Override
+            public Image next() {
+                return imageAt((i + 1) % files.length);
+            }
+
+            @Override
+            public Image prev() {
+                return imageAt((i - 1 + files.length) % files.length);
+            }
+        };
+    }
+}
